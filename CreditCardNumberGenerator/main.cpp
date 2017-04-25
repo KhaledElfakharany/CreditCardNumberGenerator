44c57095163846d3ee62d8195d1bdfb4f646ffbe
KhaledElfakharany <K.fakharany@gmail.com>
2017-04-25 16:00:07 +0200

    Initial Commit

---
+++ CreditCardNumberGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CreditCardNumberGenerator
+//
+//  Created by Khaled Elfakharany on 4/25/17.
+//  Copyright © 2017 Khaled Elfakharany. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
